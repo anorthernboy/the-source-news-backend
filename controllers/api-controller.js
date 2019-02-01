1577ab6392d0c2cf478429d30818e447c4e1a2ac
craigweighman <craigweighman@gmail.com>
2019-02-01 11:22:07 +0000

    endpoint description added

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,10 +1,15 @@
 exports.getEndpoints = (req, res, next) => {
   res.status(200).json({
-    cw_news: {
-      1: 'topics',
-      2: 'users',
-      3: 'articles',
-      4: 'comments',
+    endpoints: {
+      '/api/topics': 'responds with an array of topic objects',
+      '/api/topics/:topic/articles': 'responds with an array of article objects for the given topic',
+      '/api/users': 'responds with an array of user objects',
+      '/api/users/:username': 'responds with a username object for the given username',
+      '/api/users/:username/articles': 'responds with an array of article objects for the given username',
+      '/api/articles': 'responds with an array of article objects',
+      '/api/articles/:article_id': 'responds with an article object for the given article_id',
+      '/api/articles/:article_id/comments': 'responds with an array of comment objects for the given article_id',
+      '/api/comments': 'responds with an array of comment objects',
     },
   });
 };
