532763a69dca35e10da455fc74f79affe1ce54d8
craigweighman <craigweighman@gmail.com>
2019-02-02 20:18:12 +0000

    general 404 error handling

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -8,9 +8,7 @@ const {
 
 exports.getTopics = (req, res, next) => {
   fetchTopics()
-    .then(topics => res.status(200).json({
-      topics,
-    }))
+    .then(topics => res.status(200).json(topics))
     .catch(next);
 };
 
