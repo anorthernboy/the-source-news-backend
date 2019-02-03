12fc73717ed4d81354f720087bfc9c39841f6675
craigweighman <craigweighman@gmail.com>
2019-02-03 10:53:25 +0000

    final update for heroku hosting

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
-const knex = require('knex');
-const dbConfig = require('../knexfile');
+const ENV = process.env.NODE_ENV || 'development';
 
-const connection = knex(dbConfig);
+const config = ENV === 'production' ? {
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+} : require('../knexfile');
 
-module.exports = connection;
+module.exports = require('knex')(config);
