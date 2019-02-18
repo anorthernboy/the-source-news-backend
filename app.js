e785f312609c23bb0a0ecedd629f9e60300cffa7
craigweighman <craigweighman@gmail.com>
2019-02-18 12:54:48 +0000

    ready for deployment

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const app = require('express')();
-const bodyParser = require('body-parser');
-const apiRouter = require('./routes/api-route');
+const app = require("express")();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const apiRouter = require("./routes/api-route");
 const {
   handle400,
   handle404,
   handle405,
   handle422,
-  handle500,
-} = require('./errors/index');
-const {
-  send404,
-} = require('./errors/index');
+  handle500
+} = require("./errors/index");
+const { send404 } = require("./errors/index");
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use('/', apiRouter);
-app.use('/api', apiRouter);
-app.use('/*', send404);
+app.use("/", apiRouter);
+app.use("/api", apiRouter);
+app.use("/*", send404);
 app.use(handle400);
 app.use(handle404);
 app.use(handle405);
